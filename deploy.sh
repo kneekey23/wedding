2734a877b75b715458507122502a221247aa5264
Klein <nickik@88e9fe53292e.ant.amazon.com>
2019-01-23 23:45:31 -0800

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-npm run-script build
-cd build
-aws s3 cp . s3://nickijustinsite/ --acl public-read --region us-west-2 --profile default --recursive
+npm run build && aws s3 sync /build s3://nickijustinsite/ --acl public-read --profile default
+#aws s3 cp . s3://nickijustinsite/ --acl public-read --region us-west-2 --profile default --recursive
 aws cloudfront create-invalidation --distribution-id E1B8Z1MNDXL4AI --paths /index.html /error.html
